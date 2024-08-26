66fc7a337444fcd87deee0e144165d46d7a70bca
Pedro Lemos <lemozxpedro@gmail.com>
2024-08-26 20:35:26 -0300

    refactor: improved example

--- example/android/app/src/main/kotlin/com/example/unicons_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.unicons_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,91 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:unicons/unicons.dart';
 
-void main() => runApp(UniconsExample());
+void main() => runApp(const UniconsExample());
 
 class UniconsExample extends StatelessWidget {
+  const UniconsExample({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light().copyWith(primaryColor: Color(0xFF6666FF)),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF6666FF),
+          foregroundColor: Colors.white,
+        ),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF6666FF),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Unicons App',
-      home: IconsPage(),
+      home: const IconsPage(),
     );
   }
 }
 
 class IconsPage extends StatelessWidget {
-  final _iconsLine = [
-    UniconsLine.apps,
-    UniconsLine.cog,
-    UniconsLine.user,
-    UniconsLine.schedule,
-    UniconsLine.phone_volume,
-    UniconsLine.trophy,
-    UniconsLine.edit,
-    UniconsLine.bed,
-    UniconsLine.basketball_hoop,
-    UniconsLine.airplay,
-    UniconsLine.heartbeat,
-    UniconsLine.suitcase,
-    UniconsLine.comment_exclamation,
-    UniconsLine.padlock,
-    UniconsLine.usd_circle,
-    UniconsLine.map_pin,
-    UniconsLine.image_edit,
-    UniconsLine.money_stack,
-    UniconsLine.file_alt,
-    UniconsLine.bus_alt,
-    UniconsLine.book_open,
-    UniconsLine.credit_card,
-    UniconsLine.grin,
-    UniconsLine.newspaper,
-    UniconsLine.accessible_icon_alt,
-    UniconsLine.calculator_alt,
-    UniconsLine.unlock_alt,
-    UniconsLine.search_alt,
-    UniconsLine.package,
-    UniconsLine.tablet,
-    UniconsLine.english_to_chinese,
-    UniconsLine.bars,
-    UniconsLine.battery_empty,
-    UniconsLine.android_alt,
-    UniconsLine.home,
-    UniconsLine.scroll,
-    UniconsLine.car,
-    UniconsLine.paperclip,
-    UniconsLine.university,
-    UniconsLine.medal,
-    UniconsLine.image_upload,
-    UniconsLine.mars,
-    UniconsLine.fahrenheit,
-    UniconsLine.bill,
-    UniconsLine.bitcoin_circle,
-    UniconsLine.camera,
-    UniconsLine.gold,
-    UniconsLine.ninja,
-  ];
+  const IconsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Unicons Line'),
+        title: const Text('Unicons Line'),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 30.0),
-        shrinkWrap: true,
-        itemCount: _iconsLine.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 6),
-        itemBuilder: (context, index) => Icon(
-          _iconsLine.elementAt(index),
-          size: 50.0,
-          color: Theme.of(context).primaryColor,
+      body: Center(
+        child: Wrap(
+          spacing: 8,
+          runSpacing: 8,
+          children: uniconsLine
+              .map(
+                (icon) => Icon(
+                  icon,
+                  size: 50.0,
+                  color: colorScheme.primary,
+                ),
+              )
+              .toList(),
         ),
       ),
     );
   }
 }
+
+const uniconsLine = [
+  UniconsLine.apps,
+  UniconsLine.cog,
+  UniconsLine.user,
+  UniconsLine.schedule,
+  UniconsLine.phone_volume,
+  UniconsLine.trophy,
+  UniconsLine.edit,
+  UniconsLine.bed,
+  UniconsLine.basketball_hoop,
+  UniconsLine.airplay,
+  UniconsLine.heartbeat,
+  UniconsLine.suitcase,
+  UniconsLine.comment_exclamation,
+  UniconsLine.padlock,
+  UniconsLine.usd_circle,
+  UniconsLine.map_pin,
+  UniconsLine.image_edit,
+  UniconsLine.money_stack,
+  UniconsLine.file_alt,
+  UniconsLine.bus_alt,
+  UniconsLine.book_open,
+  UniconsLine.credit_card,
+  UniconsLine.grin,
+  UniconsLine.newspaper,
+  UniconsLine.accessible_icon_alt,
+  UniconsLine.calculator_alt,
+  UniconsLine.unlock_alt,
+  UniconsLine.search_alt,
+  UniconsLine.package,
+  UniconsLine.tablet,
+  UniconsLine.english_to_chinese,
+  UniconsLine.bars,
+  UniconsLine.battery_empty,
+  UniconsLine.android_alt,
+  UniconsLine.home,
+  UniconsLine.scroll,
+  UniconsLine.car,
+  UniconsLine.paperclip,
+  UniconsLine.university,
+  UniconsLine.medal,
+  UniconsLine.image_upload,
+  UniconsLine.mars,
+  UniconsLine.fahrenheit,
+  UniconsLine.bill,
+  UniconsLine.bitcoin_circle,
+  UniconsLine.camera,
+  UniconsLine.gold,
+  UniconsLine.ninja,
+];
